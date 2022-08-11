241a7fd68467256586026b1e9cbca7f187955a33
Joshua Chick <josh.sendemails@gmail.com>
2022-08-11 23:16:56 +1000

    Changed version.
    Version number to check for when updating changed from 1.0.0 to 1.0.1.

--- MemoryGain.py
+++ MemoryGain.py
@@ -39,7 +39,7 @@ class MainWindow(QMainWindow):
             # Update checker.
             try:
                 html = urllib.request.urlopen("https://memorygain.app")
-                if "Version 1.0.0" not in str(html.read()):
+                if "Version 1.0.1" not in str(html.read()):
                     update_msg = QMessageBox()
                     update_msg.setFont(QFont('MS Shell Dlg 2', settings.get_font_size()))
                     update_msg.setWindowTitle("Update")
