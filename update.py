7dacd60c3e7ce85cc1a8eda1405e12d1a119f9f0
Joshua Chick <josh.sendemails@gmail.com>
2022-08-16 18:47:15 +1000

    Added update_available() and go_to_memorygain_site().

---
+++ update.py
@@ -0,0 +1,29 @@
+import urllib
+import os
+
+
+def update_available():
+    """
+    Returns True if a newer version is available at https://memorygain.app.
+    """
+    try:
+        html = urllib.request.urlopen("https://memorygain.app")
+        html = str(html.read())
+        if ("Version 1.0.5" not in html) and ("MemoryGain" in html):
+            return True
+
+    except urllib.error.URLError as e:
+        print(e)
+
+    except urllib.error.HTTPError as e:
+        print(e)
+
+    return False
+
+
+def go_to_memorygain_site():
+    """
+    Opens https://memorygain.app and exits.
+    """
+    os.system("START https://memorygain.app")
+    sys.exit()
