3e31c9af969b64e3ff01f8b0f4b5f3edd70309bf
Joshua Chick <josh.sendemails@gmail.com>
2023-05-11 03:31:55 +1000

    Changed background of add_cards_ans_text scroll bar.

--- MemoryGain.py
+++ MemoryGain.py
@@ -889,11 +889,14 @@ class MainWindow(QMainWindow):
             self.add_cards_ans_text.setFont(QFont('MS Shell Dlg 2', settings.get_font_size()))
             self.add_cards_ans_text.setObjectName('add_cards_ans_text')
             self.add_cards_ans_text.setStyleSheet('''
-                                                                #add_cards_ans_text{
-                                                                    background-color: rgba(255, 255, 255, 0.1);
-                                                                    border: none;
-                                                                    color: white;
-                                                                }
+                                                    #add_cards_ans_text{
+                                                        background-color: rgba(255, 255, 255, 0.1);
+                                                        border: none;
+                                                        color: white;
+                                                    }
+                                                    QScrollBar{
+                                                       background-color: rgba(255, 255, 255, 0.1);
+                                                    }
                         ''')
             self.add_cards_ans_text.setTabChangesFocus(True)
             self.main_frame_grid_layout.addWidget(self.add_cards_ans_text, 2, 0, 1, 1)
